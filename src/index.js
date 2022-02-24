c1d55e80f9bf8992778a58a6fff05191fdbfe2c8
mjmorrison10 <86391005+mjmorrison10@users.noreply.github.com>
2022-02-23 18:24:59 -0800

    update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const game = document.querySelector(".game-board");
 const scoreBoard = document.querySelector(".game");
 
 let gameArr = [];
+let currentPlayer = 1;
+let playerHand = 0;
 // const gameArr = [20, 1, 2, 3, 4, 5, 6, 20, 7, 8, 9, 10, 11, 12];
 // const gameArr = [20, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0];
 
@@ -15,14 +17,28 @@ function playMove() {
     zone.addEventListener("click", (e) => {
       const gameIndex = gameArr[i];
       const t = e.target;
-      if (gameIndex === 1) return;
+      if (gameIndex === 0) return;
       if (t.classList.contains(`zone`)) {
-        i === 13
-          ? (gameArr[i]--, gameArr[0]++)
-          : (gameArr[i]--, gameArr[i + 1]++);
+        playerHand = gameArr[i];
+        if (i === 13) {
+          gameArr[i] = 0;
+          console.log("hand ->", playerHand);
+
+          // gameArr[i]--;
+          // gameArr[0]++;
+        } else {
+          gameArr[i] = 0;
+
+          // gameArr[i]--;
+          // gameArr[i + 1]++;
+          console.log("hand ->", playerHand);
+        }
+
+        // ? (gameArr[i]--, gameArr[0]++, console.log(playerHand))
+        // : (gameArr[i]--, gameArr[i + 1]++, console.log(playerHand));
 
         updateGame();
-        console.log(`Zone ${i}`, "gameIndex-->", gameIndex, "i -->", i);
+        // console.log(`Zone ${i}`, "gameIndex-->", gameIndex, "i -->", i);
       }
       // if (t.classList.contains(`zone-${i}`)) {
       //   console.log(`Zone ${i}`, "gameIndex-->", gameIndex, "i -->", i);
@@ -46,10 +62,16 @@ playMove();
 function updateGame() {
   const player1 = document.querySelector(".player-1");
   const player2 = document.querySelector(".player-2");
+  const hand = document.querySelector(".curPlayer-score");
+  const handPlayerEl = document.querySelector(".curPlayer");
   const zones = document.querySelectorAll(".zone");
 
   player1.querySelector("span").innerText = gameArr[0];
   player2.querySelector("span").innerText = gameArr[7];
+  hand.innerText = playerHand;
+  handPlayerEl.innerHTML = `Player ${currentPlayer}`;
+
+  // hand.insertAdjacentText("beforebegin", "Seeds in Player Hand:");
 
   zones.forEach((zone, i) => {
     gameArr[i] === 0 ? (zone.innerHTML = "") : (zone.innerHTML = gameArr[i]);
@@ -58,7 +80,8 @@ function updateGame() {
 }
 
 function init() {
-  gameArr = [20, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0];
+  // gameArr = [0, 4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4];
+  gameArr = [0, 3, 3, 3, 3, 3, 3, 0, 3, 3, 3, 3, 3, 3];
   // gameArr = [20, 1, 2, 3, 4, 5, 6, 20, 8, 9, 10, 11, 12, 13];
   updateGame();
 }
